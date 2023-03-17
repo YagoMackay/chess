60da3f4938176fb2a124ff56da2c6adb23332823
Yago Mackay <yagomackay@outlook.com>
2023-03-18 02:58:47 +1100

    piece component

--- components/Piece.tsx
+++ components/Piece.tsx
@@ -1,19 +1,52 @@
 import { Center } from '@chakra-ui/react';
 import Image from 'next/image';
+import { DragPreviewImage, useDrag } from 'react-dnd';
 
 export interface PieceProps {
+  piece: {
+    type: string;
+    color?: string;
+  };
   square?: string;
-  pieceType?: string;
-  color?: string;
+
+  position: string;
 }
-const Piece = ({ pieceType, square, color }: PieceProps) => {
-  console.log('piece', pieceType, color);
-  const pieceImg = require(`../public/images/assets/${pieceType}_${color}.png`);
+
+interface PieceDragObject {
+  id: string;
+  type: string;
+}
+const Piece = ({
+  piece: { type, color },
+  square,
+
+  position,
+}: PieceProps) => {
+  const [{ isDragging }, drag, dragPreview] = useDrag({
+    item: { id: `${position}_${type}_${color}` },
+    type: 'piece',
+    collect: (monitor) => {
+      return { isDragging: !!monitor.isDragging() };
+    },
+  });
+  const pieceImg = require(`../public/images/assets/${type}_${color}.png`);
 
   return (
-    <Center cursor={'grab'} width={'100%'} height="100%" display={'flex'}>
-      <Image src={pieceImg} alt="chess-piece" width={50} height={50}></Image>
-    </Center>
+    <>
+      <DragPreviewImage connect={dragPreview} src={pieceImg} />
+      <Center
+        cursor={'grab'}
+        width={'100%'}
+        height="100%"
+        display={'flex'}
+        ref={drag}
+        alignItems={'center'}
+        justifyContent={'center'}
+        style={{ opacity: isDragging ? 0 : 1 }}
+      >
+        <Image src={pieceImg} alt="chess-piece" width={45} height={45}></Image>
+      </Center>
+    </>
   );
 };
 export default Piece;
