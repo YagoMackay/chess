41bc37d5b2130528ac465cc2aa6f2082acfbb931
Yago Mackay <yagomackay@outlook.com>
2023-03-18 02:58:54 +1100

    boardSq comp

--- components/BoardSquare.tsx
+++ components/BoardSquare.tsx
@@ -1,22 +1,48 @@
+import { gameSubject, handleMove } from '@/lib/Game';
 import { Box } from '@chakra-ui/layout';
+import { useEffect, useState } from 'react';
+import { useDrop } from 'react-dnd';
 import Piece from './Piece';
+import Promote from './Promote';
 import Square from './Square';
 
 export type PieceProps = {
   piece?: {
-    type?: string;
+    type: string;
     square?: string;
     color?: string;
   };
-
+  position: string;
   black?: boolean;
 };
-const BoardSquare = ({ piece, black }: PieceProps) => {
-  console.log('piece', piece);
+const BoardSquare = ({ piece, black, position }: PieceProps) => {
+  console.log('pos', position);
+  const [promotion, setPromotion] = useState(null);
+  const [, drop] = useDrop({
+    accept: 'piece',
+    drop: (item: any) => {
+      const [fromPosition] = item.id.split('_');
+
+      handleMove(fromPosition, position);
+    },
+  });
+  useEffect(() => {
+    const subscribe = gameSubject.subscribe(({ pendingPromotion }: any) =>
+      pendingPromotion && pendingPromotion.to === position
+        ? setPromotion(pendingPromotion)
+        : setPromotion(null)
+    );
+    return () => subscribe.unsubscribe();
+  }, [position]);
+
   return (
-    <Box width={'100%'} height={'100%'}>
+    <Box width={'100%'} height={'100%'} ref={drop}>
       <Square black={black}>
-        {piece && <Piece pieceType={piece.type} color={piece.color}></Piece>}
+        {promotion ? (
+          <Promote promotion={promotion} />
+        ) : piece ? (
+          <Piece piece={piece} position={position}></Piece>
+        ) : null}
       </Square>
     </Box>
   );
