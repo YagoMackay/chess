cf34490253cf5baa677746c6fe475b6eb2793ac8
Yago Mackay <yagomackay@outlook.com>
2023-03-21 00:37:45 +1100

    remove logs

--- functions/boardFunctions.ts
+++ functions/boardFunctions.ts
@@ -11,17 +11,14 @@ export interface XYPosition {
 }
 
 export const getXYPosition = ({ i, turn }: GetPieceDetailProps): XYPosition => {
-  console.log('passed down', i, turn);
   const x = turn === 'w' ? i % 8 : Math.abs((i % 8) - 7);
   const y = turn === 'w' ? Math.abs(Math.floor(i / 8) - 7) : Math.floor(i / 8);
   return { x, y };
 };
 
 export const isBlack = ({ i, turn }: GetPieceDetailProps): boolean => {
-  console.log('passed down', i, turn);
   const { x, y } = getXYPosition({ i, turn });
 
-  console.log('x and y', { x, y });
   return (x + y) % 2 === 1;
 };
 
