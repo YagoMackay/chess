38560204fcee63b9093f42e59553f4b7469eb653
Yago Mackay <yagomackay@outlook.com>
2023-03-21 00:37:13 +1100

    wrap component with Chakra Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '@/styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <Component {...pageProps} />
-    </DndProvider>
+    <ChakraProvider>
+      <DndProvider backend={HTML5Backend}>
+        <Component {...pageProps} />
+      </DndProvider>
+    </ChakraProvider>
   );
 }
