c8d00b279f3920a8ee407fea0d83e0a8b2055b3f
Yago Mackay <yagomackay@outlook.com>
2023-03-22 11:14:52 +1100

    add comments to game

--- lib/Game.tsx
+++ lib/Game.tsx
@@ -19,62 +19,93 @@ export let gameSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 let member: any;
 let gameRef: any;
 
+/**
+ * Initializes the game, loading game data from Firestore if a game reference is provided.
+ * If not, the game will be initialized with a new chess board and the game state will be updated.
+ * @param gameRefFb Optional reference to the game in Firestore
+ * @returns Returns a string indicating the current state of the game initialization process
+ */
 export async function initGame(gameRefFb?: any) {
+  // Get the currently signed-in user from Firebase authentication
   const { currentUser } = auth;
 
+  // If a game reference is provided, use it to load the game data from Firestore
   if (gameRefFb) {
+    // Store the game reference for later use
     gameRef = gameRefFb;
 
+    // Load the game data from Firestore
     const initialGame: { member: Member[]; status: string } | undefined =
       await gameRefFb
         .get()
         .then((doc: firebase.firestore.QueryDocumentSnapshot) => doc.data());
+
+    // If the game data does not exist, return 'notfound'
     if (!initialGame) {
       return 'notfound';
     }
 
+    // Find the creator of the game
     const creator = initialGame.member.find((m: Member) => m.creator === true);
 
-    if (initialGame.status === 'waiting' && creator.uid !== currentUser?.uid) {
+    // If the game status is 'waiting' and the current user is not the creator, add the current user to the game
+    if (initialGame.status === 'waiting' && creator?.uid !== currentUser?.uid) {
+      // Create a new member object for the current user
       const currUser = {
         uid: currentUser?.uid,
         name: localStorage.getItem('userName'),
-        piece: creator.piece === 'w' ? 'b' : 'w',
+        piece: creator?.piece === 'w' ? 'b' : 'w',
       };
 
+      // Add the current user to the game members and update the game status to 'ready'
       const updatedMembers = [...initialGame.member, currUser];
 
       await gameRefFb.update({ member: updatedMembers, status: 'ready' });
-    } else if (
+    }
+    // If the current user is not a member of the game, return 'intruder'
+    else if (
       !initialGame.member.map((m: Member) => m.uid).includes(currentUser?.uid)
     ) {
       return 'intruder';
     }
+
     chess.reset();
 
-    //not sure how to fix this
+    // Create a new gameSubject and use the game reference to listen for changes in the game data
+    // This allows the game state to be automatically updated when changes occur in Firestore
     //@ts-ignore
     gameSubject = fromDocRef(gameRefFb).pipe(
+      // Map the game data to the game state object used by the application
       map((gameDoc) => {
         const game = gameDoc.data();
+
+        // Find the member object for the current user
         member = game?.member.find((m: Member) => m.uid === currentUser?.uid);
 
+        // Extract the gameData and pendingPromotion properties from the game object
         const { pendingPromotion, gameData, ...restOfGame }: any = game;
 
-        const oponent = game?.member.find(
+        // Find the opponent's member object
+        const opponent = game?.member.find(
           (m: Member) => m.uid !== currentUser?.uid
         );
+
+        // Load the game data into the chess engine if it exists
         if (gameData) {
           chess.load(gameData);
         }
+
+        // Determine if the game is over
         const isGameOver = chess.isGameOver();
+
+        // Return the game state object
         return {
           board: chess.board(),
           pendingPromotion,
           isGameOver,
           position: member.piece,
           member,
-          oponent,
+          opponent,
           result: isGameOver ? getGameResult() : null,
           ...restOfGame,
         };
@@ -116,7 +147,7 @@ export const move = (from: string, to: string, promotion?: any) => {
 
 export function handleMove(from: string, to: string) {
   const promotions = chess.moves({ verbose: true }).filter((m) => m.promotion);
-  console.table(promotions);
+
   let pendingPromotion;
   if (promotions.some((p) => `${p.from}:${p.to}` === `${from}:${to}`)) {
     pendingPromotion = { from, to, color: promotions[0].color };
@@ -140,17 +171,26 @@ export const resetGame = async () => {
 
 async function updateGame(pendingPromotion?: any, reset?: any) {
   const isGameOver = chess.isGameOver();
+
+  // If a game reference exists, update the game data in Firestore
   if (gameRef) {
+    // Prepare the updated data to be stored in Firestore
     const updatedData = {
       gameData: chess.fen(),
       pendingPromotion: pendingPromotion || null,
     };
 
+    // If the reset flag is true, update the game status to 'over'
     if (reset) {
       updatedData.status = 'over';
     }
+
+    // Update the game data in Firestore
     await gameRef.update(updatedData);
-  } else {
+  }
+  // If no game reference exists, save the game data locally and update the game subject
+  else {
+    // Prepare the game data to be saved locally and sent to the game subject
     const newGame = {
       board: chess.board(),
       pendingPromotion,
@@ -158,7 +198,11 @@ async function updateGame(pendingPromotion?: any, reset?: any) {
       position: chess.turn(),
       result: isGameOver ? getGameResult() : null,
     };
+
+    // Save the game data locally
     localStorage.setItem('savedGame', chess.fen());
+
+    // Update the game subject with the new game data
     gameSubject.next(newGame);
   }
 }
